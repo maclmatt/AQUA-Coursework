4113e79016dc8dc57bd81719587da9edb443901c
maclmatt <mayaclmatthews@gmail.com>
2022-11-27 11:42:34 +0000

    main menu start

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-# This is a template. 
-# You should modify the functions below to match
-# the signatures determined by the project specification
+import sys
 
 
 def main_menu():
-    """Your documentation goes here"""
-    # Your code goes here
+    """Gets and implements users choice from main menu
+    Parameters: None
+    Returns: None"""
+
+    print("Menu:")
+    print("R - Access the Pollution Reporting module\n" + 
+        "I - Access the Mobility Intelligence module\n" + 
+        "M - Access the Real-time Monitoring module\n" + 
+        "A - Print the About text\n" +
+        "Q - Quit the application")
+    option = input("Please choose one of the above: ")
+
+    if option == "R":
+        print(" ")
+        #may need to do secondary menu here, also will need to import all the functions from reporting.py
+    elif option == "I":
+        intelligence_menu()
+    elif option == "M":
+        monitoring_menu()
+    elif option == "A":
+        about()
+    elif option == "Q":
+        quit()
+
+
 
 def monitoring_menu():
     """Your documentation goes here"""
@@ -21,11 +42,11 @@ def about():
     # Your code goes here
 
 def quit():
-    """Your documentation goes here"""
-    # Your code goes here
-
-
-
+    """Quits program, with goodbye message
+    Parameters: None
+    Results: None"""
+    print("Thank you for using the AQUA platform.")
+    sys.exit()
 
 if __name__ == '__main__':
     main_menu()
